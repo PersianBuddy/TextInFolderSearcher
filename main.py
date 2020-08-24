c2faeec59bf0f0d42e843ec6cca333700f48cd11
PersianBuddy <mnajafi0014@gmail.com>
2020-08-24 16:54:45 +0430

    Add .gitignore to ignore sample direcotry

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 # TODO: get desired text from user
+user_text = input("Please enter your desired text to search here: ")
 # TODO: search all ".txt" files inside current directory
 # TODO: return Files and location where text had been found
