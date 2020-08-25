379965e110152b6d4e7a0d3fd30e388992f6524a
PersianBuddy <mnajafi0014@gmail.com>
2020-08-25 11:37:45 +0430

    Upadate program to work for multiple level files

--- main.py
+++ main.py
@@ -2,17 +2,21 @@ import os
 # get desired text from user
 user_text = input("Please enter your desired text to search here: ")
 
-current_directory = os.path.curdir
-directorys_list = os.listdir(current_directory)
+directorys_list = [os.path.abspath(os.path.curdir)]
 files_with_text = f'These files contains the word "{user_text}":\n\n'
 
 # search all ".txt" files inside current directory
-for directory_name in directorys_list:
-    if directory_name.endswith(".txt"):
-        tmp_directory = os.path.join(current_directory, directory_name)
-        text_file = open(tmp_directory, 'r')
-        if user_text.lower() in text_file.read().lower():
-            files_with_text += f'File name:\tsample.txt\n File Direcotry: \t{current_directory}\n\n'
+while directorys_list:
+    current_directory = directorys_list.pop()
+    direcoty_content = os.listdir(current_directory)
+    for directory_name in direcoty_content:
+        if directory_name.endswith(".txt"):
+            file_direcoty = os.path.join(current_directory, directory_name)
+            text_file = open(file_direcoty, 'r')
+            if user_text.lower() in text_file.read().lower():
+                files_with_text += f'File name:\t{directory_name}\n File Direcotry: \t{current_directory}\n\n'
+        elif os.path.isdir(os.path.join(current_directory, directory_name)):
+            directorys_list.append(os.path.join(current_directory, directory_name))
 
 # return Files and location where text had been found
 print(files_with_text)
